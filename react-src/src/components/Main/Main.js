d15854e8f04c295cf7d9a44e5234717f09a17365
metamn <bartus.csongor@gmail.com>
2019-07-20 11:47:53 +0300

    Refactoring

--- react-src/src/components/Main/Main.js
+++ react-src/src/components/Main/Main.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Menu from "../Menu";
-import Content, { ContentPropTypes, ContentDefaultProps } from "../Content";
+import Content, { ContentPropTypes } from "../Content";
 
 /**
  * Defines the prop types
