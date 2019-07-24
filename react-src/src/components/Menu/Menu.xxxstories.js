a6452b2f5cfee022b1cd7af157baef5a2253db49
metamn <bartus.csongor@gmail.com>
2019-07-24 14:58:54 +0300

    Identified the Storybook error cause: Menu.stories.js

--- react-src/src/components/Menu/Menu.xxxstories.js
+++ react-src/src/components/Menu/Menu.xxxstories.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
+import { CategoriesDefaultProps } from "../Categories";
 import Menu from "./Menu";
 import description from "./Menu.md";
 
-storiesOf("Menu", module).add("Overview", () => <Menu />, {
+storiesOf("Menu", module).add("Overview", () => <Menu categories={{}} />, {
   notes: { markdown: description }
 });
