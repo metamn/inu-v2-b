0b520204403cde4a47c97d9dcd18921df5346fd6
metamn <bartus.csongor@gmail.com>
2019-08-06 16:47:01 +0300

    Fixing stories

--- react-src/src/components/Categories/Categories.stories.js
+++ react-src/src/components/Categories/Categories.stories.js
@@ -1,2 +1,28 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+
+import Categories, {
+  CategoriesPropTypes,
+  CategoriesDefaultProps
+} from "./Categories";
+import description from "./Categories.md";
+
+const CategoriesForStory = () => {
+  const data = Categories({});
+  const code = JSON.stringify(data, null, 2);
+  return (
+    <>
+      <p>Loads Categories from the database</p>
+      <pre>
+        <code>{code}</code>
+      </pre>
+    </>
+  );
+};
+
+CategoriesForStory.propTypes = CategoriesPropTypes;
+CategoriesForStory.defaultProps = CategoriesDefaultProps;
+
+storiesOf("Categories", module).add("Overview", () => <CategoriesForStory />, {
+  notes: { markdown: description }
+});
--- react-src/src/components/Content/Content.stories.js
+++ react-src/src/components/Content/Content.stories.js
@@ -8,6 +8,10 @@ storiesOf("Content", module)
   .add("With default props", () => <Content />, {
     notes: { markdown: description }
   })
-  .add("With thumbs", () => <Content activeContentDisplayMode="thumbs" />, {
-    notes: { markdown: description }
-  });
+  .add(
+    "With slider",
+    () => <Content activeContentDisplayMode="slider" activeMenuItem="1" />,
+    {
+      notes: { markdown: description }
+    }
+  );
