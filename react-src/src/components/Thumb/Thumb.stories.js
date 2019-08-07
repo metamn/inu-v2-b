022989d04048b13a9b634ce5d0c9d43a045570a9
metamn <bartus.csongor@gmail.com>
2019-08-07 18:50:52 +0300

    Finishing up all stories with images

--- react-src/src/components/Thumb/Thumb.stories.js
+++ react-src/src/components/Thumb/Thumb.stories.js
@@ -4,10 +4,52 @@ import { storiesOf } from "@storybook/react";
 import Thumb from "./Thumb";
 import description from "./Thumb.md";
 
+/**
+ * Displays a real image.
+ * The default image will have visibility set to 0 so we can't use it here...
+ */
+const defaultProps = {
+  id: "1",
+  title: "Post",
+  index: 1,
+  featuredImageTitle: "Featured image",
+  featuredImageType: "large",
+  index: 1,
+  featuredImage: {
+    id: "YXR0YWNobWVudDozMw==",
+    sourceUrl: "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1.jpg",
+    mediaDetails: {
+      file: "2012/06/bogdan1.jpg",
+      height: 450,
+      width: 700,
+      sizes: [
+        {
+          file: "bogdan1-150x150.jpg",
+          height: "150",
+          mimeType: null,
+          name: "thumbnail",
+          sourceUrl:
+            "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1-150x150.jpg",
+          width: "150"
+        },
+        {
+          file: "bogdan1-300x192.jpg",
+          height: "192",
+          mimeType: null,
+          name: "medium",
+          sourceUrl:
+            "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1-300x192.jpg",
+          width: "300"
+        }
+      ]
+    }
+  }
+};
+
 storiesOf("Thumb", module)
-  .add("Inactive (default)", () => <Thumb />, {
+  .add("Inactive (default)", () => <Thumb post={defaultProps} />, {
     notes: { markdown: description }
   })
-  .add("Active", () => <Thumb isActive={true} />, {
+  .add("Active", () => <Thumb post={defaultProps} isActive={true} />, {
     notes: { markdown: description }
   });
--- react-src/src/components/Thumbs/Thumbs.stories.js
+++ react-src/src/components/Thumbs/Thumbs.stories.js
@@ -3,11 +3,54 @@ import { storiesOf } from "@storybook/react";
 
 import Thumbs from "./Thumbs";
 import description from "./Thumbs.md";
-import { PostsDefaultProps } from "../Posts";
+
+/**
+ * Displays a real image.
+ * The default image will have visibility set to 0 so we can't use it here...
+ */
+const defaultProps = {
+  id: "1",
+  title: "Post",
+  index: 1,
+  featuredImageTitle: "Featured image",
+  featuredImageType: "large",
+  index: 1,
+  featuredImage: {
+    id: "YXR0YWNobWVudDozMw==",
+    sourceUrl: "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1.jpg",
+    mediaDetails: {
+      file: "2012/06/bogdan1.jpg",
+      height: 450,
+      width: 700,
+      sizes: [
+        {
+          file: "bogdan1-150x150.jpg",
+          height: "150",
+          mimeType: null,
+          name: "thumbnail",
+          sourceUrl:
+            "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1-150x150.jpg",
+          width: "150"
+        },
+        {
+          file: "bogdan1-300x192.jpg",
+          height: "192",
+          mimeType: null,
+          name: "medium",
+          sourceUrl:
+            "http://www.inu.ro/wp-content/uploads/2012/06/bogdan1-300x192.jpg",
+          width: "300"
+        }
+      ]
+    }
+  }
+};
+
+const edgesWithFeaturedImage = Array(1).fill({ node: defaultProps });
 
 storiesOf("Thumbs", module).add(
   "Overview",
-  () => <Thumbs {...PostsDefaultProps} />,
+  () => <Thumbs edges={edgesWithFeaturedImage} />,
   {
     notes: { markdown: description }
   }
